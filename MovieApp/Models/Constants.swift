c8ad0177e927b4f876d2ec99129e3f1794869640
Anıl <anil@happygamecompany.com>
2023-03-27 04:07:11 +0300

    -Keys added

--- MovieApp/Models/Constants.swift
+++ MovieApp/Models/Constants.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 struct Constants{
-    static let tmdbApiKey = "blablabla"
+    static let tmdbApiKey = "3f8ec5368b2e60736953b918ab7bdacd"
     static let tmdbBaseUrl = "https://api.themoviedb.org/3/"
     static let tmdbPosterBaseUrl = "https://image.tmdb.org/t/p/w500/"
     
-    static let youtubeApiKey = "blablabla"
+    static let youtubeApiKey = "AIzaSyAO319yxelyYBua9q5dc5DA1XQxd7NIV7E"
     static let youtubeBaseUrl = "https://youtube.googleapis.com/youtube/v3/"
     
     static let suggestionMovieName = "Into The Wild"
